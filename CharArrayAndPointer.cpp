7d471f15b3b26270c9dab11da72aca7ac4a214e2
Bo Li <li.unicorn@gmail.com>
2015-06-16 16:21:42 +0200

    Update Char[] and Char*

--- CharArrayAndPointer.cpp
+++ CharArrayAndPointer.cpp
@@ -14,15 +14,24 @@ void charArrayAndPointer(){
     std::cout<< q << "\t" << *q << std::endl;
 
     *q = 'x';
-    std::cout<< q[0] /*<< ++q*/ /*would crash here! */
+    /*++q;*/ /*This wont compile!*/
+    std::cout<< q[0] << *(q+1)  << ++(*q)
              << q[2] << q[3] <<std::endl;
+    std::cout<< q[0] << q[1] << q[2] << q[3] <<std::endl;
     std::cout<< q << "\t" << *q << std::endl;
 
     std::cout<<"===  char*  ==="<<std::endl;
 
     char* p = "abcd";
-    std::cout<< p[0] << p[1] << p[3] << ++p <<std::endl;
+    std::cout<< p[0] << p[2] << p[3] << ++p <<std::endl;
     std::cout<< p << "\t" << *p << std::endl;
+
+    char* p2 = "bcd";
+    char* p3 = "abcd";
+    ++p3;
+    std::cout << (p == p2) <<  std::endl;
+    std::cout << (p == p3) <<  std::endl;
+
 
     *p = 'x';//would crash here!
     std::cout<< p[0] << p[1] << p[2] << p[3] <<std::endl;
